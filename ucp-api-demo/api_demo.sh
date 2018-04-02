452ce97952e27a3c71c402796bc288140433b5fb
Xinfeng Liu <xinfeng.liu@gmail.com>
2018-04-02 21:42:45 +0800

    add shell version

---
+++ ucp-api-demo/api_demo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Note: set env with UCP client bundle before running this program
+# if you are on Mac, please follow https://stackoverflow.com/questions/40712352/curl-error-58-ssl-cant-load-the-certificate-and-its-private-key-osstat to use curl with openssl support
+
+CURL=/usr/local/opt/curl/bin/curl # installed by brew
+
+if [ -z ${DOCKER_CERT_PATH} -o -z ${DOCKER_HOST} ]; then
+  echo "Error: Please set env with UCP client bundle before running this program"
+  echo "Exit."
+  exit 1
+fi
+
+${CURL} -s \
+     --cert ${DOCKER_CERT_PATH}/cert.pem \
+     --key ${DOCKER_CERT_PATH}/key.pem \
+     --cacert ${DOCKER_CERT_PATH}/ca.pem \
+     ${DOCKER_HOST/tcp/https}/"$*"
+
+# example: "$0 api/banner"
